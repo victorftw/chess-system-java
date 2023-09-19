8216807c1ee766bff454ea2f0607292ca15dd690
Victor Hugo <victor.hiraiftw@outlook.com>
2023-09-19 15:53:56 -0300

    feat: class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,22 @@
 package application;
 
+import boardgame.Position;
+
+/**
+ * This is an empty Java program It serves as a starting point for your application.
+ */
+
 public class Program {
 
+  /**
+   * The main method that drives the program execution.
+   *
+   * @param args The command-line arguments (unused).
+   */
+
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    System.out.println("Hello World!");
+    Position position = new Position(3, 5);
+    System.out.println(position);
   }
 
 }
