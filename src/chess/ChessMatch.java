bbdb52ee591594a53aebf64b7f108757b198bfc4
Victor Hugo <victor.hiraiftw@outlook.com>
2023-09-20 14:53:42 -0300

    feat: class king, rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 /**
  * Represents a chess match, including the chessboard and its pieces.
@@ -12,6 +15,7 @@ public class ChessMatch {
 
   public ChessMatch() {
     board = new Board(8, 8);
+    initialSetup();
   }
 
   /**
@@ -29,4 +33,11 @@ public class ChessMatch {
     }
     return mat;
   }
+
+  private void initialSetup() {
+    board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+    board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+    board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+  }
+
 }
---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,22 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+/**
+ * Represents a Rook chess piece.
+ */
+
+public class Rook extends ChessPiece {
+
+  public Rook(Board board, Color color) {
+    super(board, color);
+  }
+
+  @Override
+  public String toString() {
+    return "R";
+  }
+
+}
