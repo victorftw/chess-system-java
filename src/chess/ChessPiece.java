0229f29a0272b1cc1473f2ba636c8a13ab746588
Victor Hugo <victor.hiraiftw@outlook.com>
2023-09-27 10:54:04 -0300

    feat: implementing possible moves of rook

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
 /**
  * Represents a chess piece, which is a specific kind of piece on the chess board.
@@ -18,6 +19,11 @@ public abstract class ChessPiece extends Piece {
 
   public Color getColor() {
     return color;
+  }
+
+  protected boolean isThereOpponentPiece(Position position) {
+    ChessPiece p = (ChessPiece) getBoard().piece(position);
+    return p != null && p.getColor() != color;
   }
 
 }
